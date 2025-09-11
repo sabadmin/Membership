426d46105ab750b0af7dc7d4d2bde564bfc39ae3
sabadmin <sab@unfc.it>
2025-09-11 10:45:15 -0400

    dues - refferal relationship with users v3

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship, backref
 from datetime import datetime
 
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64))
