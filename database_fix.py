3f89dbd8b2231e81d68e06859b109eac8002a0d7
sabadmin <sab@unfc.it>
2025-09-11 17:07:16 -0400

    fix datbase v2

--- database_fix.py
+++ database_fix.py
@@ -40,14 +40,10 @@ DB_CONFIG = {
     'password': 'Bellm0re'
 }
 
-# List of tenant databases to fix
+# List of databases to fix - focused on liconnects and admin panel (main website)
 TENANT_DATABASES = [
-    'tenant1_db',
-    'tenant2_db',
-    'website_db',
-    'closers_db',
     'liconnects_db',
-    'lieg_db'
+    'website_db'
 ]
 
 # SQL commands to fix the database schema
